3c67162d35d85493fba515966f8413c639983f93
ribbon-12o8 <ribbon.cst@gmail.com>
2026-06-02 00:50:53 +0900

    修正

--- script.js
+++ script.js
@@ -64,3 +64,15 @@ fontSelect.addEventListener(
 );
 
 updatePreview();
+
+const submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click", async () => {
+
+  const canvas = await html2canvas(previewCard);
+
+  const image = canvas.toDataURL("image/png");
+
+  console.log(image);
+
+});
